6cdc07548b08ec7e3324f83ebf78940011f26c0c
samanthapb01 <slp87@duke.edu>
2024-11-22 10:37:23 -0500

    Import whale location csv

--- MainScript.py
+++ MainScript.py
@@ -3,5 +3,22 @@
 #Duke University Nicholas School of the Environment
 # November 22, 2024
 
-#Import files
- 
+#Import packages
+from arcgis import GIS
+from arcgis.raster import ImageryLayer, Raster
+from arcgis.geometry import Point, filters
+from datetime import datetime, timedelta
+import pandas as pd
+import geopandas as gpd
+import arcpy
+import sys
+
+#Connect to GIS via ArcGIS Pro
+gis = GIS("home")
+
+#Set default environment
+arcpy.env.workspace = "U:\\whales"
+arcpy.env.overwriteOutput = True
+
+#Import csv
+df = pd.read_csv('U:\\whales\\Data\\gray_whale_observations.csv')
