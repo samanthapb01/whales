29d25143c80396919949ffdf4b1b755d0ac9024f
samanthapb01 <slp87@duke.edu>
2024-11-22 11:20:39 -0500

    Create geodata frame and clean data

--- MainScript.py
+++ MainScript.py
@@ -21,4 +21,22 @@ arcpy.env.workspace = "U:\\whales"
 arcpy.env.overwriteOutput = True
 
 #Import csv
-df = pd.read_csv('U:\\whales\\Data\\gray_whale_observations.csv')
+df_original = pd.read_csv('U:\\whales\\Data\\gray_whale_observations.csv')
+df = df_original[['row_id','latitude','longitude','date_time','organism_id']]
+
+#Create the geoseries from the X and Y columns
+geom = gpd.points_from_xy(x=df['longitude'],y=df['latitude'])
+
+#Convert to a geodataframe, setting the CRS to WGS 84 (wkid=4326)
+gdf_whales = gpd.GeoDataFrame(df,geometry=geom,crs=4326)
+
+#Explore the data
+gdf_whales.info()
+
+#Plot the data
+explore_plot = gdf_whales.plot(figsize=(20,15),alpha=0.6)
+
+#Remove points in Russia using masks
+mask = (gdf_whales['longitude'] < 177) & (gdf_whales['longitude'] > -169)
+gdf_whales_filtered = gdf_whales.loc[mask]
+gdf_whales_filtered.plot()
